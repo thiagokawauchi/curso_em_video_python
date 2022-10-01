22c88065742342811396c64d2450971c27464de2
thiagokawauchi <thiago.kawauchi@gmail.com>
2022-10-01 16:18:08 -0300

    ex024~ex025 added

---
+++ ex024.py
@@ -0,0 +1,4 @@
+cidade = str(input('Em que cidade você nasceu? ')).strip().upper()
+print(f'Nome da cidade começa com "Santo": {cidade.startswith("SANTO")}')
+
+# print(f'Nome da cidade começa com "Santo": {cidade[0:5] == "SANTO"}')
---
+++ ex025.py
@@ -0,0 +1,2 @@
+nome_completo = str(input('Qual é seu nome completo? ')).strip().upper()
+print(f'Seu nome tem "Silva"? {"SILVA" in nome_completo}')
